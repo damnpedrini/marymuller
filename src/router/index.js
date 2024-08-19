ed3ff9d92e8c3092ae9555b821ee52f428e79f30
ChrystianWGR <c.ramos@isulpar.edu.br>
2024-08-19 20:35:28 -0300

    atualizacao

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '.public/logo.png'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
